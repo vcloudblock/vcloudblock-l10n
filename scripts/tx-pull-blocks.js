5eb69fd0d2c799bdb7551086987654b52a155d24
Arthur Zheng <869705086@qq.com>
2021-01-17 00:28:24 +0800

    Change name to openblock

--- scripts/tx-pull-blocks.js
+++ scripts/tx-pull-blocks.js
@@ -30,7 +30,7 @@ import async from 'async';
 import locales, {localeMap} from '../src/supported-locales.js';
 
 // Globals
-const PROJECT = 'scratch-editor-2';
+const PROJECT = 'openblock-editor';
 const RESOURCE = 'blocks';
 const OUTPUT_DIR = path.resolve(args[0]);
 const MODE = {mode: 'reviewed'};
